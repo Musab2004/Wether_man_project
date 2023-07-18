90a53e6d6b1b7e63822e6347806821cd8e7a825d
Musab <l200920@lhr.nu.edu.pk>
2023-07-18 18:52:48 +0500

    final_version

--- weatherman_project.py
+++ weatherman_project.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[132]:
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,32 +6,30 @@ import os
 import pandas as pd
 import sys
 import re
+import math
+
+first_col_name = "Time_Zone"
 
 
+def check_format_Scenario_2_3(user_input):
+    pattern = r"^\d{4}/\d{1,2}$"
 
-
-def check_format_2(user_input):
-    pattern = r'^\d{4}/\d{1,2}$'
-     
     if re.match(pattern, user_input):
-       year, month = user_input.split('/')
-       if int(month)<=12 and int(month)>=1:
-            # print("here",month)
+        year, month = user_input.split("/")
+        if int(month) <= 12 and int(month) >= 1:
+       
             return str(user_input)
-        
+
     print(f"Invalid Input.")
     sys.exit()
 
 
-
-
-def check_format_1(user_input):
-    pattern = r'^\d{4}$'
+def check_format_Scenario_1(user_input):
+    pattern = r"^\d{4}$"
 
     if re.match(pattern, user_input):
-          
-            return str(user_input)
-        
+        return str(user_input)
+
     print(f"Invalid input.")
     sys.exit()
 
@@ -45,29 +39,22 @@ def openfolder(directory):
         print(f"The directory '{directory}' does not exist.")
         sys.exit()
     else:
-        # Use glob to get the list of text files
-        # print("now here")
-        print(directory)
-        # base_name = os.path.basename(directory)
-        # print(base_name)
-        if "Dubai" in directory:
-           first_col_name='GST'          
-        elif "lahore" or "Murree" in directory:
-            first_col_name='PKT'
-        text_files = glob.glob(directory + '*.txt')
+        text_files = glob.glob(directory + "*.txt")
 
-
-    i=0
+    i = 0
     for file_path in text_files:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             data_frame = pd.read_csv(file_path)
-            if i==0:
-             appended_df = data_frame
-            
+            data_frame.rename(
+                columns={data_frame.columns[0]: "Time_Zone"}, inplace=True
+            )
+            if i == 0:
+                appended_df = data_frame
+
             else:
-             appended_df= pd.concat([appended_df, data_frame], ignore_index=True)   
-            i=i+1
-    return appended_df,first_col_name
+                appended_df = pd.concat([appended_df, data_frame], ignore_index=True)
+            i = i + 1
+    return appended_df
 
 
 def openfile(directory):
@@ -75,20 +62,9 @@ def openfile(directory):
         print(f"The directory '{directory}' does not exist.")
         sys.exit()
     else:
-        base_name = os.path.basename(directory)
-       
-        if "Dubai" in base_name:
-           first_col_name='GST'
-
-        elif "lahore" or "Murree" in base_name:
-            first_col_name='PKT'
-        text_files = glob.glob(directory + '*.txt')
-        # Use glob to get the list of text files
         data_frame = pd.read_csv(directory)
-    return data_frame,first_col_name        
-# numeric_value = get_fixed_length_numeric_input(6)
-
-
+        data_frame.rename(columns={data_frame.columns[0]: "Time_Zone"}, inplace=True)
+    return data_frame
 
 
 def check_choice(user_input):
@@ -99,146 +75,148 @@ def check_choice(user_input):
     print("Invalid input.")
     sys.exit()
 
-def filter_dataframe(appended_df,user_input,first_col_name):
-    appended_df[first_col_name].fillna("",inplace=True)
-    appended_df.columns = [c.replace(' ', '_') for c in appended_df.columns]
 
+def filter_data_frame(appended_df, user_input):
+    appended_df[first_col_name].fillna("", inplace=True)
+    appended_df.columns = [c.replace(" ", "_") for c in appended_df.columns]
 
     mask = appended_df[first_col_name].str.contains(user_input)
     at_least_one_true = mask.any()
-    # print(at_least_one_true)
-    # filtered_df = appended_df[mask]
-    # print(filtered_df)
-    # all_false = mask.all()
-    # print(mask)
     if not at_least_one_true:
         print("Year or month doesn't exist in the data ")
         sys.exit()
-        
-
 
     filtered_df = appended_df[mask]
     return filtered_df
 
 
-
-def case1(filtered_df,first_col_name):
+def Extreme_Weather_Report_in_a_year(filtered_df):
     months_dict = {
-    1: 'Jan',
-    2: 'Feb',
-    3: 'Mar',
-    4: 'Apr',
-    5: 'May',
-    6: 'Jun',
-    7: 'Jul',
-    8: 'Aug',
-    9: 'Sep',
-    10: 'Oct',
-    11: 'Nov',
-    12: 'Dec'
-}
-    
-    highest_row = filtered_df.loc[filtered_df['Max_TemperatureC'].idxmax()]
-    year,month,day=highest_row[first_col_name].split('-')
-    print("Date : ",months_dict[int(month)],day,"Highest : ",highest_row['Max_TemperatureC'])
-    highest_row = filtered_df.loc[filtered_df['Min_TemperatureC'].idxmin()]
-    year,month,day=highest_row[first_col_name].split('-')
-    print("Date : ",months_dict[int(month)],day,"Lowest : ",highest_row['Min_TemperatureC'])
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec",
+    }
 
-    highest_row = filtered_df.loc[filtered_df['Max_Humidity'].idxmax()]
-    year,month,day=highest_row[first_col_name].split('-')
-    
-    print("Date : ",months_dict[int(month)],day,"Humid : ",highest_row['Max_Humidity'])
+    highest_row = filtered_df.loc[filtered_df["Max_TemperatureC"].idxmax()]
+    year, month, day = highest_row[first_col_name].split("-")
+    print(
+        "Highest : ",
+        highest_row["Max_TemperatureC"],
+        "C ",
+        "On ",
+        months_dict[int(month)],
+        day,
+    )
+    highest_row = filtered_df.loc[filtered_df["Min_TemperatureC"].idxmin()]
+    year, month, day = highest_row[first_col_name].split("-")
+    print(
+        "Lowest : ",
+        highest_row["Min_TemperatureC"],
+        "C ",
+        "On ",
+        months_dict[int(month)],
+        day,
+    )
 
+    highest_row = filtered_df.loc[filtered_df["Max_Humidity"].idxmax()]
+    year, month, day = highest_row[first_col_name].split("-")
 
-
-
-def case2(filtered_df,first_col_name):
-    Max_temp_mean = filtered_df['Max_TemperatureC'].mean()
-    print("Max temp:",Max_temp_mean)
-    Min_temp_mean = filtered_df['Min_TemperatureC'].mean()
-    print("Min Temp mean :",Min_temp_mean)
-
-
-    humidity = filtered_df['Max_Humidity'].mean()
-    print("Humidity Mean: ",humidity)
-
-
-
-
-def case3(filtered_df,first_col_name):
-        GST = filtered_df[first_col_name]
-        Max_Temprature = filtered_df['Max_TemperatureC']
-        Min_Temprature = filtered_df['Min_TemperatureC']
-
-        # Set the height of the bars
-        bar_height = 0.35
-
-        # Set the positions of the bars on the y-axis
-        y = np.arange(len(GST))
-
-        # Create the horizontal bar plot for Max Temperature
-        plt.barh(y, Max_Temprature, height=bar_height, label='Max Temperature')
-
-        # Shift the positions of the bars for Min Temperature
-        y = y + bar_height
-
-        # Create the horizontal bar plot for Min Temperature
-        plt.barh(y, Min_Temprature, height=bar_height, label='Min Temperature')
-
-        # Set the labels and title
-        plt.ylabel('GST')
-        plt.xlabel('Max and Min Temperatures')
-        plt.title('Horizontal Bar Plot of Max and Min Temperatures')
-
-        plt.yticks(y - bar_height / 2, GST)
-
-        # Add a legend
-        plt.legend()
-
-        # Display the plot
-        plt.show()
+    print(
+        "Humid : ",
+        highest_row["Max_Humidity"],
+        "% ",
+        "On ",
+        months_dict[int(month)],
+        day,
+    )
 
 
-# Main        
-user_input=sys.argv[1]
+def Average_Weathre_Report_in_a_month(filtered_df):
+    Max_temp_mean = filtered_df["Max_TemperatureC"].mean()
+    print("Max temp:", Max_temp_mean, "C")
+    Min_temp_mean = filtered_df["Min_TemperatureC"].mean()
+    print("Min Temp mean :", Min_temp_mean, "C")
 
-directory=sys.argv[2]
-choice = str(sys.argv[3])
-choice = check_choice(choice)
+    humidity = filtered_df["Max_Humidity"].mean()
+    print("Humidity Mean: ", humidity, "%")
 
-# print(user_input)
 
-if choice==1:
- check_format_1(user_input)
- df,first_col_name=openfolder(directory)
-
- df=filter_dataframe(df,user_input,first_col_name)
- case1(df,first_col_name)
- 
-elif choice==2:
- check_format_2(user_input)
- user_input=user_input.replace("/","-")
- df,first_col_name=openfile(directory)
- df=filter_dataframe(df,user_input,first_col_name)
- case2(df,first_col_name)
-elif choice==3:
- check_format_2(user_input)
- user_input=user_input.replace("/","-")
- df,first_col_name=openfile(directory)
- df=filter_dataframe(df,user_input,first_col_name)
- case3(df,first_col_name)    
+def Bar_Graph_Weather_Report(filtered_df):
+    GST = filtered_df[first_col_name]
+    Max_Temprature = filtered_df["Max_TemperatureC"]
+    Min_Temprature = filtered_df["Min_TemperatureC"]
+    color_red = "\033[91m"
+    color_blue = "\033[94m"
+    color_reset = "\033[0m"
+    for i in range(0, len(GST)):
+        print(color_reset + GST[i], end="     ")
+  
+        if math.isnan(Max_Temprature[i]):
+       
+            pass
+        else:
+            for j in range(0, int(Max_Temprature[i])):
+                print(color_red + "+", end="")
+            print("   ", color_red + str(Max_Temprature[i]), "C", end=" ")
+        print()
+        print(color_reset + GST[i], end="     ")
+        if math.isnan(Max_Temprature[i]):
+            # print()
+            pass
+        else:
+            for j in range(0, int(Min_Temprature[i])):
+                print(color_blue + "+", end="")
+            print("   ", color_blue + str(Min_Temprature[i]), "C", end=" ")
+        print()
 
 
 
 
 
+user_input = sys.argv[2]
+
+directory = sys.argv[3]
+choice = str(sys.argv[1])
+
+num_args = len(sys.argv) - 1
+if num_args !=3:
+    print("invalid input format")
+    sys.exit()
 
 
+if choice == "-a":
+    check_format_Scenario_1(user_input)
+    df = openfolder(directory)
 
+    df = filter_data_frame(df, user_input)
 
+    Extreme_Weather_Report_in_a_year(df)
 
+elif choice == "-e":
+    check_format_Scenario_2_3(user_input)
+    user_input = user_input.replace("/", "-")
 
+    df = openfile(directory)
 
+    df = filter_data_frame(df, user_input)
 
+    Average_Weathre_Report_in_a_month(df)
+elif choice == "-c":
+    check_format_Scenario_2_3(user_input)
+    user_input = user_input.replace("/", "-")
+    df = openfile(directory)
+    df = filter_data_frame(df, user_input)
+    Bar_Graph_Weather_Report(df)
+else:
+    print("Wrong flag")
 
+print()
