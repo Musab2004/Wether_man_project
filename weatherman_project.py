7e1c5a4a38db9687f29c651cbe8fb2ea850fec39
Musab <l200920@lhr.nu.edu.pk>
2023-07-19 14:40:20 +0500

    Final_version

--- weatherman_project.py
+++ weatherman_project.py
@@ -7,11 +7,37 @@ import pandas as pd
 import sys
 import re
 import math
-
+from datetime import datetime
 first_col_name = "Time_Zone"
-
-
-def check_format_scenario_2_3(user_input):
+months_dict = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec",
+    }
+month_dict = {
+        "Jan": 1,
+        "Feb": 2,
+        "Mar": 3,
+        "Apr": 4,
+        "May": 5,
+        "Jun": 6,
+        "Jul": 7,
+        "Aug": 8,
+        "Sep": 9,
+        "Oct": 10,
+        "Nov": 11,
+        "Dec": 12
+    }
+def check_date_format(user_input):
     pattern = r"^\d{4}/\d{1,2}$"
     try:
         if re.match(pattern, user_input):
@@ -29,7 +55,7 @@ def check_format_scenario_2_3(user_input):
         sys.exit()
 
 
-def check_format_scenario_1(user_input):
+def check_date_month_format(user_input):
     pattern = r"^\d{4}$"
     try:
         if re.match(pattern, user_input):
@@ -41,7 +67,7 @@ def check_format_scenario_1(user_input):
         sys.exit()
 
 
-def openfolder(directory):
+def open_folder(directory):
     try:
         if not os.path.exists(directory):
             raise FileNotFoundError(f"The directory '{directory}' does not exist.")
@@ -74,7 +100,7 @@ def openfolder(directory):
         sys.exit()
 
 
-def openfile(directory):
+def open_file(directory):
     try:
         if not os.path.exists(directory):
             raise FileNotFoundError(f"The directory '{directory}' does not exist.")
@@ -107,37 +133,24 @@ def check_choice(user_input):
         sys.exit()
 
 
-def filter_data_frame(appended_df, user_input):
-    appended_df[first_col_name].fillna("", inplace=True)
-    appended_df.columns = [c.replace(" ", "_") for c in appended_df.columns]
+def filter_data_frame(appended_data_frame, user_input):
+    appended_data_frame[first_col_name].fillna("", inplace=True)
+    appended_data_frame.columns = [c.replace(" ", "_") for c in appended_data_frame.columns]
 
-    mask = appended_df[first_col_name].str.contains(user_input)
+    mask = appended_data_frame[first_col_name].str.contains(user_input)
     at_least_one_true = mask.any()
     if not at_least_one_true:
         print("Year or month doesn't exist in the data ")
         sys.exit()
 
-    filtered_df = appended_df[mask]
+    filtered_df = appended_data_frame[mask]
     return filtered_df
 
 
-def extreme_weather_report_in_year(filtered_df):
-    months_dict = {
-        1: "Jan",
-        2: "Feb",
-        3: "Mar",
-        4: "Apr",
-        5: "May",
-        6: "Jun",
-        7: "Jul",
-        8: "Aug",
-        9: "Sep",
-        10: "Oct",
-        11: "Nov",
-        12: "Dec",
-    }
+def extreme_weather_report_in_year(filtered_data_frame):
 
-    highest_row_max_temp = filtered_df.loc[filtered_df["Max_TemperatureC"].idxmax()]
+
+    highest_row_max_temp = filtered_data_frame.loc[filtered_data_frame["Max_TemperatureC"].idxmax()]
     try:
         year, month, day = highest_row_max_temp[first_col_name].split("-")
 
@@ -157,7 +170,7 @@ def extreme_weather_report_in_year(filtered_df):
         print("Error: Missing key in the 'highest_row' dictionary.")
     except ValueError as e:
         print("Error: Invalid month value.")
-    highest_row_min_temp = filtered_df.loc[filtered_df["Min_TemperatureC"].idxmin()]
+    highest_row_min_temp = filtered_data_frame.loc[filtered_data_frame["Min_TemperatureC"].idxmin()]
     try:
         year, month, day = highest_row_min_temp[first_col_name].split("-")
 
@@ -177,7 +190,7 @@ def extreme_weather_report_in_year(filtered_df):
     except ValueError as e:
         print("Error: Invalid month value.")
 
-    highest_row_max_humid = filtered_df.loc[filtered_df["Max_Humidity"].idxmax()]
+    highest_row_max_humid = filtered_data_frame.loc[filtered_data_frame["Max_Humidity"].idxmax()]
 
     # year, month, day = highest_row[first_col_name].split("-")
     try:
@@ -230,14 +243,37 @@ def bar_graph_weather_report(filtered_df):
         print()
         print(color_reset + time_zone[i], end="     ")
         if math.isnan(max_temprature[i]):
-            # print()
             pass
         else:
             for j in range(0, int(min_temprature[i])):
                 print(color_blue + "+", end="")
             print("   ", color_blue + str(min_temprature[i]), "C", end=" ")
         print()
+def check_input(date_string,file_path):
 
+    match = re.search(r"(\d{4})_(\w+)\.txt", file_path)
+
+    if match:
+        year = int(match.group(1))
+        month_name = match.group(2)
+        if month_name in month_dict:
+            month = month_dict[month_name]
+
+            # Extract month and year from the date string
+            date_parts = date_string.split("/")
+            comparison_year = int(date_parts[0])
+            comparison_month = int(date_parts[1])
+
+            # Compare month and year
+            if year == comparison_year and month == comparison_month:
+                pass
+            else:
+                print("File path and Date given do not match")
+                sys.exit()
+        else:
+            print("Invalid month name")
+    else:
+        print("No match found")
 
 num_args = len(sys.argv) - 1
 if num_args != 3:
@@ -250,27 +286,29 @@ choice = str(sys.argv[1])
 
 try:
     if choice == "-a":
-        check_format_scenario_1(user_input)
-        data_frame = openfolder(directory)
+        check_date_month_format(user_input)
+        data_frame = open_folder(directory)
 
         data_frame = filter_data_frame(data_frame, user_input)
 
         extreme_weather_report_in_year(data_frame)
 
     elif choice == "-e":
-        check_format_scenario_2_3(user_input)
+        check_input(user_input,directory)
+        check_date_format(user_input)
         user_input = user_input.replace("/", "-")
 
-        data_frame = openfile(directory)
+        data_frame = open_file(directory)
 
         data_frame = filter_data_frame(data_frame, user_input)
 
         average_weather_report_in_month(data_frame)
 
     elif choice == "-c":
-        check_format_scenario_2_3(user_input)
+        check_input(user_input,directory)
+        check_date_format(user_input)
         user_input = user_input.replace("/", "-")
-        data_frame = openfile(directory)
+        data_frame = open_file(directory)
         data_frame = filter_data_frame(data_frame, user_input)
         bar_graph_weather_report(data_frame)
 
