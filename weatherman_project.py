2408b9bc0ffab7bc177691243fafc76a9ef2d493
Musab <l200920@lhr.nu.edu.pk>
2023-07-19 09:30:35 +0500

    added try and excpet and change function names accordingly

--- weatherman_project.py
+++ weatherman_project.py
@@ -42,36 +42,53 @@ def check_format_scenario_1(user_input):
 
 
 def openfolder(directory):
-    if not os.path.exists(directory):
-        print(f"The directory '{directory}' does not exist.")
+    try:
+        if not os.path.exists(directory):
+            raise FileNotFoundError(f"The directory '{directory}' does not exist.")
+        else:
+            text_files = glob.glob(directory + "*.txt")
+
+        i = 0
+        for file_path in text_files:
+            with open(file_path, "r") as file:
+                data_frame = pd.read_csv(file_path)
+                data_frame.rename(
+                    columns={data_frame.columns[0]: "Time_Zone"}, inplace=True
+                )
+                if i == 0:
+                    appended_data_frame = data_frame
+                else:
+                    appended_data_frame = pd.concat([appended_data_frame, data_frame], ignore_index=True)
+                i = i + 1
+
+        return appended_data_frame
+    
+    except FileNotFoundError as e:
+        print(str(e))
         sys.exit()
-    else:
-        text_files = glob.glob(directory + "*.txt")
 
-    i = 0
-    for file_path in text_files:
-        with open(file_path, "r") as file:
-            data_frame = pd.read_csv(file_path)
-            data_frame.rename(
-                columns={data_frame.columns[0]: "Time_Zone"}, inplace=True
-            )
-            if i == 0:
-                appended_df = data_frame
-
-            else:
-                appended_df = pd.concat([appended_df, data_frame], ignore_index=True)
-            i = i + 1
-    return appended_df
+    except Exception as e:
+        print("An error occurred:", "Wrong directory")
+        sys.exit()
 
 
 def openfile(directory):
-    if not os.path.exists(directory):
-        print(f"The directory '{directory}' does not exist.")
-        sys.exit()
-    else:
+    try:
+        if not os.path.exists(directory):
+            raise FileNotFoundError(f"The directory '{directory}' does not exist.")
+        
         data_frame = pd.read_csv(directory)
         data_frame.rename(columns={data_frame.columns[0]: "Time_Zone"}, inplace=True)
-    return data_frame
+        
+        return data_frame
+    
+    except FileNotFoundError as e:
+        print(str(e))
+        sys.exit()
+    
+    except Exception as e:
+        print("An error occurred:", "Wrong Directory")
+        sys.exit()
 
 
 def check_choice(user_input):
